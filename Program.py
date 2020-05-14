ea30c1921370da4848266d43248d3bc65ad9fd93
Brandon-Clark189 <b.norton5@students.clark.edu>
2020-05-13 19:51:06 -0700

    added code

--- Program.py
+++ Program.py
@@ -1,11 +1,36 @@
-# Program.py
-
-from MyLibrary import *
-
+"""
+CTEC 121
+Brandon Norton
+Module 4 Lab 2 Demo
+"""
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+from graphics import *
 def main():
-    print("calling functions in library")
-    print("__name__:", __name__)
-    function1()
-    function2()
-
-main()
+    '''
+    startValue = 1
+    endValue = 10
+    sum = 0
+    if startValue % 2 == 1:
+        startValue = startValue + 1
+    for i in range(startValue, endValue, 2):
+        sum = sum + i
+    print(sum)
+    '''
+    # example of setting own coordinates
+    win = GraphWin("demo", 800, 800)
+    win.setCoords(-4.0, -4.0, 4.0, 4.0)
+    p1 = Circle(Point(2, 3), 1)
+    p1.setFill("red")
+    p1.draw(win)
+    p2 = Circle(Point(-3, 1), 1)
+    p2.setFill("black")
+    p2.draw(win)
+    p3 = Circle(Point(-1.5, -2.5), 1)
+    p3.setFill("blue")
+    p3.draw(win)  
+    input()
+main()    
